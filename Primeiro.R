84ab2674d1f0c512114e1fcf92a34cbdc0ae84cf
Maria Lídia <malicote84@gmail.com>
2019-09-13 14:46:33 -0300

    Adicionei o script no arquivo Projeto.R

--- Primeiro.R
+++ Primeiro.R
@@ -4,4 +4,12 @@ print("Primeiro Projeto")
 
 # vamos usar o script
 
-A variável x é um vetor com média , variância , e desvio padrão 
+#A variável x é um vetor com média 3, variância 2,5, e desvio padrão 1.58
+
+x=c(1,2,3,4,5)
+
+mean(x)
+
+var(x)
+
+sd(x)
